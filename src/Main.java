5992e7afabe192097f96908febc6512e7ba66905
mehran-montazer <65732217+mehran-montazer@users.noreply.github.com>
2020-06-12 18:26:37 +0430

    first try

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,7 @@
 import java.io.File;
-
+//har gooneh copy bardari as in code shar'an haram ast :)
 public class Main {
     public static void main (String[] arg){
-        for (int i =0 ; i < 50 ; i ++ ){
-            System.out.println("mehran dolesh kochike \n");
-        }
-        int b = 4;
-        if (b == 4)
-            System.out.println("Mahyar khare");
-        int h =0 ;
-        boolean qv = true;
-        if (qv)
-            h = 4;
-
-
-
-        int jjjjjjj = 85 ;
-        qy = false;
+        //Rading File Section
     }
 }
