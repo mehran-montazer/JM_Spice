122915cd88db7a1a7556bf02c136ae38ba1067a7
mahyarjn <mahyarjafari.nd@gmail.com>
2020-06-15 11:35:35 +0430

    queue added in node

--- src/elements/Node.java
+++ src/elements/Node.java
@@ -1,12 +1,19 @@
 package elements;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Node {
     private String name;
     private double voltage;
+    Queue<Node> saf = new LinkedList<Node>();
     ////////////////////////////////constructor////////////////////////
     Node (String name){
         this.name = name;
         voltage = 0;
+    }
+    public void adder(Node node){
+        saf.add(node);
     }
     /////////////////////////////////getter///////////////////////////
     public String getName() {
